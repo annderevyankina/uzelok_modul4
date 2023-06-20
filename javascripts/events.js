50559ca2a56ac6452e0dcd63224c436d57001a0b
ansderevyankina <anka73der@gmail.com>
2023-06-20 16:22:53 +0300

    .

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
-
 	$('.n1').click(function(){
-    if ($(".event_1").hasClass("none")) {
-        $(".event_1").removeClass("none");
-        $(".event_1").css("animation-play-state", "running");
-      } else {
-        $(".event_1").toggleClass("none");
-      }
-
-
+    $(".event_1").removeClass("none");
+    $(".event_1").css("animation-play-state", "running");
 	});
-
+	$('.bubbles').click(function(){
+    $(".event_1").addClass("none");
+	});
 });
