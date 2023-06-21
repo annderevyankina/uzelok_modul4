33b42200a3e5b1f52785c0f8162d3face77916b8
ansderevyankina <anka73der@gmail.com>
2023-06-21 04:29:14 +0300

    shop

--- javascripts/events.js
+++ javascripts/events.js
@@ -2,8 +2,36 @@ $(document).ready(function(){
 	$('.n1').click(function(){
     $(".event_1").removeClass("none");
     $(".event_1").css("animation-play-state", "running");
+		$('h3').addClass("blur");
 	});
-	$('.bubbles').click(function(){
-    $(".event_1").addClass("none");
+	$('.n2').click(function(){
+    $(".event_2").removeClass("none");
+    $(".event_2").css("animation-play-state", "running");
+		$('h3').addClass("blur");
+	});
+	$('.n3').click(function(){
+    $(".event_3").removeClass("none");
+    $(".event_3").css("animation-play-state", "running");
+		$('h3').addClass("blur");
+	});
+	$('.n4').click(function(){
+    $(".event_4").removeClass("none");
+    $(".event_4").css("animation-play-state", "running");
+		$('h3').addClass("blur");
+	});
+	$('.n5').click(function(){
+    $(".event_5").removeClass("none");
+    $(".event_5").css("animation-play-state", "running");
+		$('h3').addClass("blur");
+	});
+	$('.n6').click(function(){
+    $(".event_6").removeClass("none");
+    $(".event_6").css("animation-play-state", "running");
+		$('h3').addClass("blur");
+	});
+	$('.n7').click(function(){
+    $(".event_7").removeClass("none");
+    $(".event_7").css("animation-play-state", "running");
+		$('h3').addClass("blur");
 	});
 });
---
+++ javascripts/shop.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	$('.n1').click(function(){
+    $(".item_1").removeClass("none");
+    $(".item_1").css("animation-play-state", "running");
+		$('h3').addClass("blur");
+	});
+	$('.n2').click(function(){
+    $(".item_2").removeClass("none");
+    $(".item_2").css("animation-play-state", "running");
+		$('h3').addClass("blur");
+	});
+	$('.n3').click(function(){
+    $(".item_3").removeClass("none");
+    $(".item_3").css("animation-play-state", "running");
+		$('h3').addClass("blur");
+	});
+	$('.n4').click(function(){
+    $(".item_4").removeClass("none");
+    $(".item_4").css("animation-play-state", "running");
+		$('h3').addClass("blur");
+	});
+	$('.n5').click(function(){
+    $(".item_5").removeClass("none");
+    $(".item_5").css("animation-play-state", "running");
+		$('h3').addClass("blur");
+	});
+	$('.n6').click(function(){
+    $(".item_6").removeClass("none");
+    $(".item_6").css("animation-play-state", "running");
+		$('h3').addClass("blur");
+	});
+	$('.n7').click(function(){
+    $(".item_7").removeClass("none");
+    $(".item_7").css("animation-play-state", "running");
+		$('h3').addClass("blur");
+	});
+});
